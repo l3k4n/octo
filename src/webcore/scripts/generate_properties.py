f408ffc62b0aaf6e6154205290aca7c948fec1fa
lekan <alowoojalekan@gmail.com>
2025-03-05 17:48:11 -0600

    refactor(css): move properties json file to src

--- src/webcore/scripts/generate_properties.py
+++ src/webcore/scripts/generate_properties.py
@@ -3,9 +3,10 @@ from jinja2 import Template
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--output_dir", type=str, required=True, help="Path to the output directory")
+parser.add_argument("--properties_file", type=argparse.FileType('r'), required=True, help="Path to the properties json file")
 args = parser.parse_args()
 os.makedirs(args.output_dir, exist_ok=True)
-properties = json.load(open("css_properties.json", "r"))
+properties = json.load(args.properties_file)
 
 longhands = [property for property in properties if property.get("is_longhand")]
 open(os.path.join(args.output_dir, "longhands.h"), "w").write(
--- src/webcore/scripts/generate_property_id.py
+++ src/webcore/scripts/generate_property_id.py
@@ -3,9 +3,10 @@ from jinja2 import Template
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--output_dir", type=str, required=True, help="Path to the output directory")
+parser.add_argument("--properties_file", type=argparse.FileType('r'), required=True, help="Path to the properties json file")
 args = parser.parse_args()
 os.makedirs(args.output_dir, exist_ok=True)
-properties = json.load(open("css_properties.json", "r"))
+properties = json.load(args.properties_file)
 
 open(os.path.join(args.output_dir, "propertyid.h"), "w").write(
     Template(open("templates/css_property_id.h.txt").read()).render({ "properties": properties })
